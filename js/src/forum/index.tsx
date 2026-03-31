6be75c45d427f43a33ffc4d6a7570c6bea36582a
ResofireV2 <stryguardian@gmail.com>
2026-03-31 16:39:58 -0500

    Add files via upload

--- js/src/forum/index.tsx
+++ js/src/forum/index.tsx
@@ -1,10 +1,10 @@
 import { extend } from 'flarum/common/extend';
 import app from 'flarum/forum/app';
 import IndexPage from 'flarum/forum/components/IndexPage';
-import Search from 'flarum/forum/components/Search';
+import GlobalSearch from 'flarum/forum/components/GlobalSearch';
 
 app.initializers.add('resofire-mobile-search', () => {
   extend(IndexPage.prototype, 'viewItems', function (items) {
-    items.add('search', <Search state={app.search.state} />, -100);
+    items.add('search', <GlobalSearch state={app.search.state} />, -100);
   });
 });
