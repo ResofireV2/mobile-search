7838a61ecb86b90bfa495336f276021c327ae50b
ResofireV2 <stryguardian@gmail.com>
2026-03-31 17:05:19 -0500

    Add files via upload

--- js/src/forum/index.tsx
+++ js/src/forum/index.tsx
@@ -2,21 +2,40 @@ import { extend } from 'flarum/common/extend';
 import app from 'flarum/forum/app';
 import IndexPage from 'flarum/forum/components/IndexPage';
 import GlobalSearch from 'flarum/forum/components/GlobalSearch';
-
-const ICON_MODE_CLASS = 'resofire-search-icon-mode';
+import Button from 'flarum/common/components/Button';
+import GlobalDiscussionsSearchSource from 'flarum/forum/components/GlobalDiscussionsSearchSource';
+import GlobalPostsSearchSource from 'flarum/forum/components/GlobalPostsSearchSource';
+import GlobalUsersSearchSource from 'flarum/forum/components/GlobalUsersSearchSource';
 
 app.initializers.add('resofire-mobile-search', () => {
   extend(IndexPage.prototype, 'viewItems', function (items) {
-    items.add('search', <GlobalSearch state={app.search.state} />, -100);
-  });
-
-  extend(IndexPage.prototype, 'oncreate', function () {
     if (app.forum.attribute('resofireMobileSearchStyle') === 'icon') {
-      $('#app').addClass(ICON_MODE_CLASS);
+      items.add(
+        'search',
+        <Button
+          className="Button Button--icon"
+          icon="fas fa-search"
+          aria-label={app.translator.trans('core.forum.header.search_placeholder')}
+          title={app.translator.trans('core.forum.header.search_placeholder')}
+          onclick={() => {
+            const sources = [];
+            if (app.forum.attribute('canViewForum')) {
+              sources.push(new GlobalDiscussionsSearchSource());
+              sources.push(new GlobalPostsSearchSource());
+            }
+            if (app.forum.attribute('canSearchUsers')) {
+              sources.push(new GlobalUsersSearchSource());
+            }
+            app.modal.show(() => import('flarum/common/components/SearchModal'), {
+              searchState: app.search.state,
+              sources,
+            });
+          }}
+        />,
+        -100
+      );
+    } else {
+      items.add('search', <GlobalSearch state={app.search.state} />, -100);
     }
-  });
-
-  extend(IndexPage.prototype, 'onremove', function () {
-    $('#app').removeClass(ICON_MODE_CLASS);
   });
 });
